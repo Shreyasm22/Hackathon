95fd48d860ac81137c18559bcbb63f376f03539a
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-21 08:35:30 +0000

    updated

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -18,3 +18,19 @@ async def test_mux(dut):
     assert dut.out.value == B, "Sel line inp{A} doesn't exist".format(
             A=int(dut.sel.value), B =int(dut.inp12.value))
 
+@cocotb.test()
+async def test_mux(dut):
+    """Test for mux2"""
+
+    A = 0b01100
+    B = 0b11
+
+    dut.sel.value = A
+    dut.inp12.value = B
+
+    await Timer(2, units='ns')
+    
+    assert dut.out.value == B, "Sel line inp{A} doesn't exist".format(
+            A=int(dut.sel.value), B =int(dut.inp12.value))
+
+
