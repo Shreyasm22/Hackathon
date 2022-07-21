78a4ad6e20588e81c29461e6c26335e98f1ee20b
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-21 16:36:37 +0000

    update

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,15 +7,15 @@ from cocotb.triggers import Timer
 async def test_mux1(dut):
     """Test for mux2"""
     for i in range(0,31):
-        A = '{0:05b}'.format(i)
+        A = 0b00000 | bin(i)
         B = 0b11
 
-        dut.sel.value = '{0:05b}'.format(i)
-        dut.inp1.value = B
+        dut.sel.value = A
+        dut.inp{str(i)}.value = B
 
         await Timer(2, units='ns')
     
         dut._log.info(f'Sel line = {A:05} INP12 = {B:05}  DUT={int(dut.out.value):05}')
         assert dut.out.value == B, "Sel line inp{A} doesn't exist".format(
-                A=int(dut.sel.value), B =int(dut.inp1.value))
+                A=int(dut.sel.value), B =int(dut.inp{str(i)}.value))
 
