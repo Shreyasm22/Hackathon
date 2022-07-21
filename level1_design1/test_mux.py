a88cb294868a79262859a5235c43b010137177a4
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-21 11:01:11 +0000

    update

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,33 +6,16 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux1(dut):
     """Test for mux2"""
+    for i in range(0,31):
+        A = bin(i)
+        B = 0b11
 
-    A = 0b01101
-    B = 0b11
+        dut.sel.value = A
+        dut.inp{i}.value = B
 
-    dut.sel.value = A
-    dut.inp12.value = B
-
-    await Timer(2, units='ns')
+        await Timer(2, units='ns')
     
-    dut._log.info(f'Sel line = {A:05} INP12 = {B:05}  DUT={int(dut.out.value):05}')
-    assert dut.out.value == B, "Sel line inp{A} doesn't exist".format(
-            A=int(dut.sel.value), B =int(dut.inp12.value))
-
-@cocotb.test()
-async def test_mux2(dut):
-    """Test for mux2"""
-
-    A = 0b01100
-    B = 0b11
-
-    dut.sel.value = A
-    dut.inp12.value = B
-
-    await Timer(2, units='ns')
-    
-    dut._log.info(f'Sel line = {A:05} INP12 = {B:05}  DUT={int(dut.out.value):05}')
-    assert dut.out.value == B, "Sel line inp{A} doesn't exist".format(
-            A=int(dut.sel.value), B =int(dut.inp12.value))
-
+        dut._log.info(f'Sel line = {A:05} INP12 = {B:05}  DUT={int(dut.out.value):05}')
+        assert dut.out.value == B, "Sel line inp{A} doesn't exist".format(
+                A=int(dut.sel.value), B =int(dut.inp12.value))
 
