decc8c7cb441ba89bd90ad64ce4d6e68100bd4bc
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-21 17:12:40 +0000

    update

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -1,5 +1,4 @@
 # See LICENSE.vyoma for details
-import os
 import cocotb
 from cocotb.triggers import Timer
 
@@ -7,13 +6,12 @@ from cocotb.triggers import Timer
 async def test_mux1(dut):
     """Test for mux2"""
 
-    A = 0b00000
     for i in range(0,31):
         A = A + 1;
         B = 0b11
 
         dut.sel.value = A
-        dut.("inp" + "" + {str(i)}).value = B
+        dut.inp.value = B
 
         await Timer(2, units='ns')
     
