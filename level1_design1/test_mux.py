f4091985c2d236b089b9d0d344c1705a994fe2d3
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-21 11:06:52 +0000

    update

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -12,11 +12,11 @@ async def test_mux1(dut):
         C = i
 
         dut.sel.value = A
-        dut.inp{C}.value = B
+        dut.inp{int(C)}.value = B
 
         await Timer(2, units='ns')
     
         dut._log.info(f'Sel line = {A:05} INP12 = {B:05}  DUT={int(dut.out.value):05}')
         assert dut.out.value == B, "Sel line inp{A} doesn't exist".format(
-                A=int(dut.sel.value), B =int(dut.inp{C}.value))
+                A=int(dut.sel.value), B =int(dut.inp{int(C)}.value))
 
