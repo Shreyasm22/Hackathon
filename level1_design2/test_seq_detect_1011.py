384051522ceff11fdb8424d48996f901471d4e1e
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-21 09:55:10 +0000

    update

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -24,15 +24,19 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
     dut._log.info(f'Input= {(dut.inp_bit.value)}, DUT={bin(dut.seq_seen.value)}')
 
     dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
     dut._log.info(f'Input= {(dut.inp_bit.value)}, DUT={bin(dut.seq_seen.value)}')
     
     dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
     dut._log.info(f'Input= {(dut.inp_bit.value)}, DUT={bin(dut.seq_seen.value)}')
 
     dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
     dut._log.info(f'Input= {(dut.inp_bit.value)}, DUT={bin(dut.seq_seen.value)}')
 
     assert dut.seq_seen.value == 1, "Output sequence is not high for 1011"
