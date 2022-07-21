f864da4418d0d02cca9b69828f68239dbe10a63d
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-21 09:52:54 +0000

    update

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -23,8 +23,8 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
-    await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
     dut._log.info(f'Input= {(dut.inp_bit.value)}, DUT={bin(dut.seq_seen.value)}')
 
     await FallingEdge(dut.clk)
