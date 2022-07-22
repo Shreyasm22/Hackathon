d6166e363a572fed2a0668bbc93d752074e96ac3
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-22 08:29:09 +0000

    commit

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -9,9 +9,10 @@ async def test_mux(dut):
     for i in range(0,32):
         A = f'%5b%i'
         B = 0b11
+        C = "inp" + "" + i
 
         dut.sel.value = A
-        dut.("inp" + "" + i).value = B
+        dut.C.value = B
 
         dut._log.info(f'dut._log.info(f'Sel line = {(dut.sel.value)}, DUT={(dut.out.value)}')
         assert dut.inp{cocotb.handle.ModifiableObject(i)}.value == dut.out.value, "Output is not matching"
