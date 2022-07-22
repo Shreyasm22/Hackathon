53e88306108b5ae46c42e757c5df559834e59301
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-22 07:58:53 +0000

    commit

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -5,14 +5,12 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux1(dut):
     """Test for mux2"""
-        A = 0b00110
-        B = 0b11
+    A = 0b00110
+    B = 0b11
 
-        dut.sel.value = A
-        dut.inp11.value = B
-
-        
+    dut.sel.value = A
+    dut.inp11.value = B
     
-        dut._log.info(type(dut.inp11))
+    dut._log.info(type(dut.inp11))
         
 
