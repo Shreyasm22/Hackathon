7ee608631703be162340fff30b3ffa1a8e946145
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-22 08:55:23 +0000

    commit

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -9,12 +9,9 @@ async def test_mux(dut):
     for i in [0b00000, 0b00001, 0b00010, 0b00011]:
         A = i
         B = 0b11
-        C = int(i)
-        D = "inp" + "" + str(C) 
         dut.sel.value = A
-        dut.D.value = B
 
         dut._log.info(f'Sel = {(dut.sel.value)}, DUT={(dut.out.value)}')
-        assert dut.C.value == dut.out.value, "Output is not matching"
+        assert dut.sel.value == dut.out.value, "Output is not matching"
         
 
