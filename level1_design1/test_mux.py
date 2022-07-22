bb3cf3d93ee5ab95e9c49c2167fa54b821396ab2
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-22 08:05:50 +0000

    update

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -5,12 +5,14 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux1(dut):
     """Test for mux2"""
-    A = 0b00110
-    B = 0b11
+    for i in range(0b00000, 0b11111)
+        A = i
+        B = 0b11
 
-    dut.sel.value = A
-    dut.inp11.value = B
-    
-    dut._log.info(type(dut.inp11))
+        dut.sel.value = A
+        dut.inp1.value = B
+
+        dut._log.info(f'dut._log.info(f'Sel line = {(dut.sel.value)}, DUT={(dut.out.value)}')
+        assert dut.inp1.value == dut.out.value, "Output is not matching"
         
 
