27bf6c7d1b1019bb80a7a17a13c4f58f09f94008
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-30 17:31:30 +0000

    u

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,10 +6,13 @@ from cocotb.triggers import Timer
 async def test_mux(dut):
     """Test for mux2"""
    
-    dut.sel.value = 0b00000
-    dut.inp0.value = 0b11
+    dut.sel.value = 0b01101
+    dut.inp12.value = 0b11
+    dut.inp13.value = 0b00
     await Timer(2, units='ns')
-    assert dut.inp0.value == dut.out.value, "Output is not matching"
+    assert dut.inp12.value == dut.out.value, "Output is not matching"
+    assert dut.inp13.value == dut.out.value, "Output is not matching"
+
 
 
 
