c6633d68f7540aed3e18f1e78d5603232cbc16dc
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-30 15:28:41 +0000

    up

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,7 +8,9 @@ async def test_mux(dut):
     mux = {0b00000 : 0b11, 0b00001 : 0b00, 0b00010 : 0b11, 0b00011 : 0b00}
 
     for x in mux:
-        dut._log.info(f'Sel = {x}, Inp = {mux[x]}, DUT={(dut.out.value)}')
-        assert mux[x] == dut.out.value, "Output is not matching"
+        dut.sel.value = x
+        dut.inp{int(x)}.value = mux[x]
+        dut._log.info(f'Sel = {x} Inp = {mux[x]} DUT={(dut.out.value)}')
+        assert dut.inp{int(x)}.value == dut.out.value, "Output is not matching"
         
 
