b293d512205780f17933097b1783eb08271d6579
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-31 14:15:21 +0000

    i

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,7 +7,7 @@ async def test_mux1(dut):
     """Test for mux2"""
    
     dut.sel.value = 0b01101
-    dut.inp12.value = 0b11
+    dut.inp13.value = 0b11
     await Timer(2, units='ns')
     assert dut.inp12.value == dut.out.value, "Output is not matching"
 
