be2617961cb1154b97bd893d5ab4bfaf2bf48385
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-07-31 14:21:14 +0000

    p

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -9,6 +9,7 @@ async def test_mux1(dut):
     dut.sel.value = 0b01101
     dut.inp13.value = 0b11
     await Timer(2, units='ns')
+    dut._log.info(f'Sel = {(dut.sel.value)} Input= {(dut.inp13.value)}, DUT={(dut.out.value)}')
     assert dut.inp12.value == dut.out.value, "Output is not matching"
 
 @cocotb.test()
@@ -17,6 +18,7 @@ async def test_mux2(dut):
     dut.sel.value = 0b01000
     dut.inp8.value = 0b10
     await Timer(2, units='ns')
+    dut._log.info(f'Sel = {(dut.sel.value)} Input= {(dut.inp8.value)}, DUT={(dut.out.value)}')
     assert dut.inp8.value == dut.out.value, "Output is not matching"
 
     
