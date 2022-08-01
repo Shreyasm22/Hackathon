c236d85b8d76823d93290e0a6dec453944caf49f
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-08-01 10:18:56 +0000

    ok

--- level1_design2/Seqdet_Fail/test_seq_detect_1011.py
+++ level1_design2/Seqdet_Fail/test_seq_detect_1011.py
@@ -42,6 +42,23 @@ async def test_seq_2(dut):
     assert dut.current_state.value == 0b000, "Reset is not working" 
     assert dut.seq_seen.value == 0, "Design error"
 
+@cocotb.test()
+async def test_seq_3(dut):
+    
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk) 
+    dut.reset.value = 0
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    dut._log.info(f"Inp = {dut.inp_bit.value} State = {dut.current_state.value} Out = {dut.seq_seen.value}")
+    assert dut.current_state.value == 0b001, "State transition failed" 
+    assert dut.seq_seen.value == 0, "Design error"
+
 
 
     
