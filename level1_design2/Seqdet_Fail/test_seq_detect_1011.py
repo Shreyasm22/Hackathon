c56d4f245d3ae094b06e684321bf7527eb5988bc
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-08-01 09:59:36 +0000

    done

--- level1_design2/Seqdet_Fail/test_seq_detect_1011.py
+++ level1_design2/Seqdet_Fail/test_seq_detect_1011.py
@@ -21,8 +21,8 @@ async def test_seq_bug1(dut):
     dut.reset.value = 1
     await FallingEdge(dut.clk) 
     dut.inp_bit.value = 1
-    dut._log.info(dut.next_state.value)
-    assert dut.next_state.value == "SEQ1", "Reset is not working" 
+    dut._log.info(dut.current_state.value)
+    assert dut.current_state.value == "IDLE", "Reset is not working" 
     assert dut.seq_seen.value == 0, "Design error"
     
     
