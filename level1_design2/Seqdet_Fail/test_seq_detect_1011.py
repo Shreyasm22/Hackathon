101c13ea686d9cd9c96775832f2f5df476b40080
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-08-01 10:45:04 +0000

    ok

--- level1_design2/Seqdet_Fail/test_seq_detect_1011.py
+++ level1_design2/Seqdet_Fail/test_seq_detect_1011.py
@@ -122,7 +122,7 @@ async def test_seq_6(dut):
     assert dut.seq_seen.value == 0, "Design error"
 
 @cocotb.test()
-async def test_seq_6(dut):
+async def test_seq_7(dut):
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
