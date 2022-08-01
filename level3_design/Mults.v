b0ea423f06d2c03a5a71d855a1302fada4ea159f
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-08-01 16:10:37 +0000

    ok

--- level3_design/Mults.v
+++ level3_design/Mults.v
@@ -1,12 +1,2 @@
-module mult(eqz, ldA, ldB, ldP, clrP, decB, data_in, clk, done, start);
-inout eqz;
-inout reg ldA, ldB, ldP, clrP, decB;
-input clk, start;
-input [15:0]data_in;
-output reg done;
 
-controller C(ldA, ldB, ldP, clrP, decB, done, clk, eqz, start);
-MUL_datapath M(eqz, ldA, ldB, ldP, clrP, decB, data_in, clk);
-
-endmodule
 
