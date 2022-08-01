2e643b952b0226d459c66f65d2beccc81813994d
Shreyasm22 <Shreyas.mahesh.22@gmail.com>
2022-08-01 10:37:43 +0000

    ok

--- level1_design2/Seqdet_Fail/test_seq_detect_1011.py
+++ level1_design2/Seqdet_Fail/test_seq_detect_1011.py
@@ -81,8 +81,8 @@ async def test_seq_4(dut):
     assert dut.current_state.value == 0b010, "State transition failed" 
     assert dut.seq_seen.value == 0, "Design error"
 
+@cocotb.test()
 async def test_seq_5(dut):
-    
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
